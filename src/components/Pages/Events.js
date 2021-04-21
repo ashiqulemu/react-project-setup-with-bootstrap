39cfd03fb09b74bc781ede357482860583d4f50a
Emu <ashiqulemu.jpi@gmail.com>
2021-04-21 15:12:50 +0600

    dsf

--- src/components/Pages/Events.js
+++ src/components/Pages/Events.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'; 
-import {
-  BrowserRouter as Router,
-  Link
-} from "react-router-dom";
+import React from 'react'; 
+  
     export default function Events(){ 
 
+      function doClick(e){
+        console.log(e)
+      } 
         return( 
 
-          <h1>asdad</h1>  
+          <div>
+            <h1>CLICK EVENT</h1>
+
+            <button onClick={doClick.bind(this,'hello im parameter')}> click me </button>
+            
+          </div>
 
         )
 
